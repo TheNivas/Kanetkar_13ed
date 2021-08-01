9cb47697401fd5082679be43041c35c9b8ba0c6a
The Devilz <40209926+the-devilz@users.noreply.github.com>
2021-08-01 05:54:38 +0530

    Update E_a.c

--- ch3_Decision_control/E_a.c
+++ ch3_Decision_control/E_a.c
@@ -2,6 +2,11 @@
 
 int main()
 {
-    
+    int i = 4, z = 12;
+    if (i = 5 || z> 50)           // here i = 5 is assignment operator so if block always execute
+        printf("Dean of Students affairs \n");
+    else
+        printf("Dosa \n");   
     return 0;
+    //Output: Dean of Students affairs
 }
